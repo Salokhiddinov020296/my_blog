17fe9c3658a525a834613cb77e7d96e4a2d2f0da
Maksud <salokhiddinovmaksud@gmail.com>
2022-07-02 15:59:53 +0500

    ...

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CategoryModel, BlogsModel
 
-# Register your models here.
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+
+
+@admin.register(BlogsModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_display_links = ['title', 'created_at']
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+import datetime
+import os.path
+import random
+from io import BytesIO
 
-# Create your models here.
+from PIL import Image
+from django.core.files import File
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+def upload_to_blogs(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    filename = "{:%Y-%m-%d-%H-%M-%S}-{}{}".format(
+        datetime.datetime.now(),
+        random.randint(100000, 999999),
+        ext
+    )
+    return "{:%Y}/{:%m}/{}".format(
+        datetime.datetime.now(),
+        datetime.datetime.now(),
+        filename
+    )
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+class BlogsModel(models.Model):
+    category = models.ForeignKey(CategoryModel, on_delete=models.RESTRICT)
+    title = models.CharField(max_length=255)
+    body = RichTextUploadingField()
+    image = models.ImageField(upload_to=upload_to_blogs)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'blog'
+        verbose_name_plural = 'blogs'
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            try:
+                image_file = BytesIO(self.image.read())
+                image = Image.open(image_file)
+                image.thumbnail((500, 500), Image.ANTIALIAS)
+                image_file = BytesIO()
+                image.save(image_file, 'JPG')
+                self.image = File(image_file, name=self.image.name + '.jpg')
+            except:
+                pass
+        return super().save(*args, **kwargs)
